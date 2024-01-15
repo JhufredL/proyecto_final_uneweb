1090c567946e73ebd5ed4bcad2f28f3d27ffbd8f
alen <alenjhufred@gmail.com>
2024-01-15 14:38:23 -0400

    feat: finish project

---
+++ terminar_compra.php
@@ -0,0 +1,9 @@
+<?php
+?>
+
+<?php
+# Es responsabilidad del programador hacer algo con los productos
+include_once "funciones.php";
+$productos = obtenerProductosEnCarrito();
+# Puede que solo quieras los ids, para ello invoca a obtenerIdsDeProductosEnCarrito();
+echo "Muchas gracias por su compra!!!";
